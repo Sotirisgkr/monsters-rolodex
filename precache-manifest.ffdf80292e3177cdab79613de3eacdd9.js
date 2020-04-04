0c2c028a0c3ebcb97f2ac224c0d678bc6e507ff4
Sotirisgkr <sotirisgkr@hotmail.com>
2020-04-04 14:03:47 +0300

    Updates

---
+++ precache-manifest.ffdf80292e3177cdab79613de3eacdd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae07df32b244fbafe29f526a77831c1c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d83ee7c504130b33dfab",
+    "url": "/monsters-rolodex/static/css/main.e7174e10.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d83ee7c504130b33dfab",
+    "url": "/monsters-rolodex/static/js/main.92b1c591.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
