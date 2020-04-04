94745f0ea812f5ef47679a0d9837759d9ecae842
Sotirisgkr <sotirisgkr@hotmail.com>
2020-04-04 14:00:18 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
-
+import {SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -9,13 +9,17 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
+    // bind is a method on any function that returns a new function that the context
+    // of this is set to whatever we pass to it
+    // this.handleChange = this.handleChange.bind(this);
 
   }
 
 
-// when this component mounts, it cause whatever kind block of code we right inside
+// when this component mounts, it cause whatever kind block of code we write inside
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
@@ -23,12 +27,36 @@ class App extends Component {
   };
 
 
+  // this way is equivalent to binding in class app
+  // handleChange = (e) => {}
+    handleChange = e => {
+      this.setState({ searchField: e.target.value});
+    }
+
   render(){
+    // we want to destruct the object state
+    // we pull the values from state and we make them consts.
+    const { monsters, searchField } = this.state;
+    
+    // equivalent to const monsters = this.state.monsters;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
     return(
       <div className="App">
-        <CardList monsters={this.state.monsters}/>
-
-              
+        <h1> Monsters Rolodex </h1>
+        {/* with onchange, whenever the value changes in the search field, the onchange 
+        is called.
+        Now, because we want to display the html, we want to GET the value so we 
+        want the getTarget value so that it gives us the string value that it holds*/}
+        
+        {/* <input type='search'
+        placeholder='search monsters'
+        // because set state is an asynchronous function, we need to use a callback after 
+        // the set state to make it 
+        onChange={e => this.setState({ searchField: e.target.value})}/> */}
+        <SearchBox placeholder='search monsters' handleChange={this.handleChange}></SearchBox>
+        <CardList monsters={filteredMonsters}/>
       </div>
     )
   }
